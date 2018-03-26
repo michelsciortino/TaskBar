0c5cde3804aaa1f1c17659c6a7e739132d26ebcd
Michel Sciortino <michel.sciortino@outlook.com>
2018-03-26 03:09:31 +0200

    the Item controls container for the AppBar

---
+++ AppBar/Controls/ItemListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AppBar.Controls
+{
+    /// <summary>
+    /// Logica di interazione per BarListControl.xaml
+    /// </summary>
+    public partial class ItemListControl : UserControl
+    {
+        public ItemListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
