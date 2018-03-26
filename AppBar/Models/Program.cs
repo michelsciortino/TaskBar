51f9c9bfd85934bef29a9e3b85930cab6dd1943a
Michel Sciortino <michel.sciortino@outlook.com>
2018-03-26 03:07:16 +0200

    The Program base model

---
+++ AppBar/Models/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+
+namespace AppBar.Models
+{
+    public class Program
+    {
+        public bool IsRunning { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public BitmapImage Icon { get; set; }
+        public List<Process> ActiveProcesses { get; set; }
+    }
+}
