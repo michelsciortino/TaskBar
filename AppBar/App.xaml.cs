5b00dbb7534d34d68f26d2557304799e2d691adb
Michel Sciortino <michel.sciortino@outlook.com>
2018-03-26 03:15:35 +0200

    Changed the MainWindow apparence

--- AppBar/App.xaml.cs
+++ AppBar/App.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using AppBar.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace AppBar
 {
@@ -13,5 +14,51 @@ namespace AppBar
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Uri uri = new Uri("C:/Users/Michel/source/repos/AppBar/AppBar/Images/siren.png");
+
+            //showing the main window
+            MainWindow AppBar = new MainWindow();
+
+            //loading some test content
+            ((MainWindowViewModel)AppBar.DataContext).Items = new List<ItemViewModel>()
+            {
+                new ItemViewModel
+                {
+                    IsPinned=true,
+                    LinkedProgram = new Models.Program
+                    {
+                        Name="notepad",
+                        IsRunning=false,
+                        Icon=new BitmapImage(uri),
+                        ActiveProcesses=new List<System.Diagnostics.Process>(),
+                        Path="notepad.exe",
+                    },
+                    IconWidth = 16,
+                    IconHeight= 16,
+                    IsOpened = false
+                },
+                new ItemViewModel
+                {
+                    IsPinned=true,
+                    LinkedProgram = new Models.Program
+                    {
+                        Name="explorer",
+                        IsRunning=false,
+                        Icon=new BitmapImage(uri),
+                        ActiveProcesses=new List<System.Diagnostics.Process>(),
+                        Path="explorer.exe",
+                    },
+                    IconWidth = 16,
+                    IconHeight= 16,
+                    IsOpened = true
+                },
+            };
+
+            AppBar.Show();
+        }
+
     }
 }
