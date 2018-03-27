32630dccddb6e1f77348bf30c22633a4887734ff
Michel Sciortino <michel.sciortino@outlook.com>
2018-03-27 10:42:20 +0200

    Added Configuration Serialization/Deserialization

--- AppBar/Helpers/Serializer.cs
+++ AppBar/Helpers/Serializer.cs
@@ -3,15 +3,25 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace AppBar.Helpers
 {
-    class Serializer
+    public static class Serializer
     {
-        public void SerializeObj<T>(T serializableObj,string filename,bool append = false)
+        public static void SerializeObj<T>(T serializableObj,string filename,bool append = false)
         {
             if (serializableObj == null)
                 return;
             using (Stream stream= File.Open(filename,append? FileMode.Append: FileMode.Create))
             {
-                var binaryFormatter = new BinaryFormatter();
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(stream, serializableObj);
+            }
+        }
+
+        public static T DeserializeObj<T>(string filepath)
+        {
+            using (Stream stream = File.Open(filepath, FileMode.Open))
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
             }
         }
     }
--- AppBar/Models/Config.cs
+++ AppBar/Models/Config.cs
@@ -3,32 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace AppBar.Models
 {
+    /// <summary>
+    /// AppBar Configuration
+    /// </summary>
     [Serializable]
     public class Config
     {
-        List<Program> Programs;
-        List<string> UserIconsPaths;
+        #region Public Properties
+        /// <summary>
+        /// List of programs saved in the configuration
+        /// </summary>
+        public List<Program> Programs;
 
-        public Config(List<Program> pList,List<String> uipList)
+        /// <summary>
+        /// List of custom icons
+        /// </summary>
+        public List<BitmapImage> CustomIcons;
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="pList"> List of programs </param>
+        /// <param name="customIconList"> List of custom icons </param>
+        public Config(List<Program> pList,List<BitmapImage> customIconList)
         {
             Programs = new List<Program>(pList);
-            UserIconsPaths = new List<string>(uipList);
+            CustomIcons = new List<BitmapImage>(customIconList);
         }
 
+        #endregion
 
-        public static Config ReadConfiguration()
+        #region Public Methods
+
+        /// <summary>
+        /// Get a Configuration from file
+        /// </summary>
+        /// <param name="filename">Path + filename of configuration file</param>
+        /// <returns>A Config if the Deserialization succeeded : null otherwise</returns>
+        public static Config ReadConfiguration(string filename)
         {
             Config newConfig = null;
+
+            try
+            {
+                newConfig = Helpers.Serializer.DeserializeObj<Config>(filename);
+            }
+            catch
+            {
+                return null;
+            }
 
             return newConfig;
         }
 
-        public static bool SaveConfiguration()
+        /// <summary>
+        /// SAve a Configuration to file
+        /// </summary>
+        /// <param name="config">Configuration to be saved</param>
+        /// <param name="filename">Path + filename of configuration file</param>
+        /// <returns>True if Serialization succeeded : False otherwise</returns>
+        public static bool SaveConfiguration(Config config,string filename)
         {
+            try
+            {
+                Helpers.Serializer.SerializeObj<Config>(config, filename);
+            }
+            catch
+            {
+                return false;
+            }
             return true;
         }
+
+        #endregion
     }
 }
