3d73446cb445be14bc9578ac113e461b9658ee32
Michel Sciortino <michel.sciortino@outlook.com>
2018-03-27 12:14:59 +0200

    Created MainWindoViewModel

---
+++ Configurator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using AppBar.Core.ViewModels;
+using System.Windows.Controls;
+
+namespace Configurator.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        #region Private Properties
+
+        private Page _currentPage;
+
+        #endregion
+
+        #region Public Properties
+
+        public Page CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                if (_currentPage != value)
+                {
+                    _currentPage = value;
+                    OnPropertyChanged(nameof(CurrentPage));
+                }
+            }
+        }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            // TODO: creare la pagina delle settings
+            CurrentPage = new Page();
+        }
+
+    }
+}
